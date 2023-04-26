c54923a1c23e6f27582565189c9e844369b03b58
Vu Duyen <vuduyen145@gmail.com>
2023-04-27 00:31:09 +0700

    done

---
+++ src/Acme.BookStore.EntityFrameworkCore/Migrations/20230426165646_Added_AuthorId_To_Book.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAuthorIdToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+    name: "AuthorId",
+    table: "AppBooks",
+    type: "uniqueidentifier",
+    nullable: false,
+    defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppBooks_AuthorId",
+                table: "AppBooks",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppBooks_AppAuthors_AuthorId",
+                table: "AppBooks",
+                column: "AuthorId",
+                principalTable: "AppAuthors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
